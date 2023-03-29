fb8d424e5f4e846ab76d5eb59faf53082caa9ade
Your User Name <dicksonmwadime2020@gmail.com>
2023-03-30 00:33:17 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+// const currentUser = ' Grace Hopper ';
+// const welcomeMessage = 'welcome to Flatbook' + currentUser;
+//console.log(welcomeMessage);
+
+// const myJob = ' teacher ';
+// const myName = 'eddy';
+// const myAge = 20;
+//console.log('My name is '+ myName + ' am ' + myAge);
+
+// console.log(`my name is ${myName} and i am ${myAge} i work as a ${myJob}`);${myName}
+
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `welcome to Fla;tbook, ${currwelcomeentUser}!`
+const exitedWelcomeMessage = welcomeMessage. toUpperCase();
+const shortGreeting = ` welcome, ${currentUser.slice(0, 1)}!` ;
